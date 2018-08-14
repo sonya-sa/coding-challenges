82ed501fabc4bf6986fbb46e22c68de83cb3e365
Sonya Salimy <sonya.salimy@gmail.com>
2018-08-13 22:20:31 -0700

    latest commit

--- lecture_notes.py
+++ lecture_notes.py
@@ -1,5 +1,7 @@
 #palindrome 
 
+import random
+
 for word in ["racecar", "radar", "house"]:
     palindrome = True
     start = 0
@@ -16,3 +18,23 @@ for word in ["racecar", "radar", "house"]:
     if palindrome:
         print("palindrome: {}".format(word))
 
+def play_game():
+
+    secret_number = random.randint(1, 100)
+    guess = None
+    too_low = 0
+    too_high = 101
+    
+    print("Shh... the secret number is {}".format(secret_number))
+    
+    while secret_number != guess:
+        guess = int((too_high - too_low) / 2) + too_low
+        print("Too High = {}, Too Low = {}, Guess = {}".format(
+            too_high, too_low, guess))
+    
+        if guess > secret_number:
+            too_high = guess
+        if guess < secret_number:
+            too_low = guess
+
+play_game()
